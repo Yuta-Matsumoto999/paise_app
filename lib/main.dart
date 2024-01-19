7f414f09ad698cf78584a5fb63da4dd8c1e310ed
松本雄大 <matsumotoyuudai@matsumotoyuudainoMacBook-Air.local>
2024-01-19 22:05:43 +0900

    add supabase and riverpod settings

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:praise_app_flutter/ui/view/router/app_router.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  await dotenv.load(fileName: '.env');
+  WidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(
+    url: dotenv.get('SUPABASE_URL'),
+    anonKey: dotenv.get('SUPABASE_ANON_KEY'),
+  );
+
+  runApp(
+    const ProviderScope(child: MyApp())
+  );
 }
 
 class MyApp extends StatelessWidget {
