4ac915decb16b4e8e70dacb82e6adf27eefbb36b
松本雄大 <matsumotoyuudai@matsumotoyuudainoMacBook-Air.local>
2024-01-24 00:43:18 +0900

    add custom color

--- lib/core/constants/custom_color.dart
+++ lib/core/constants/custom_color.dart
@@ -1,4 +1,9 @@
 class CustomColor {
   // ここに追記
-  // static const mainTheme = 0x80B11E1F;
+  static const mainColor = 0xFF747CFB;
+  static const authFormBackground = 0xFFFFFFFF;
+  static const authFormBoxShadow = 0xFFEDF2F6;
+  static const authButtonText = 0xFFF8FDFB;
+  static const titleText = 0xFF6B73E2;
+  static const errorColor = 0x33ff6347;
 }
