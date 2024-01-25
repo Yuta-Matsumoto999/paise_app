97ca688fcc00798cc25a34dcd12cafdca6ae6655
松本雄大 <matsumotoyuudai@matsumotoyuudainoMacBook-Air.local>
2024-01-25 22:12:16 +0900

    create google auth error exception

--- lib/core/constants/message_ja.dart
+++ lib/core/constants/message_ja.dart
@@ -7,4 +7,5 @@ class MessageJa {
   static const internalServerError = "エラーが発生しました。しばらくお待ちださい";
   static const exceitopnError = "予期せぬエラーが発生しました。";
   static const networkError = "通信エラーが発生しました。デバイスの通信を確認してください。";
+  static const googleAuthError = "Googleアカウントでの認証に失敗しました";
 }
