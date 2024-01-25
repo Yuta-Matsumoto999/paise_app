c299f704e65e824a55c840fd3d9ad065e3b4f9bf
松本雄大 <matsumotoyuudai@matsumotoyuudainoMacBook-Air.local>
2024-01-25 22:17:12 +0900

    add google auth logic

--- lib/ui/view/components/button/google_auth_button.dart
+++ lib/ui/view/components/button/google_auth_button.dart
@@ -1,11 +1,16 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:praise_app_flutter/core/constants/custom_color.dart';
+import 'package:praise_app_flutter/ui/controller/auth/google_auth_controller.dart';
+import 'package:praise_app_flutter/ui/view/components/snackBar/error_snack_bar.dart';
+import 'package:praise_app_flutter/ui/view/router/app_router.dart';
 
-class GoogleAuthButton extends StatelessWidget {
+class GoogleAuthButton extends ConsumerWidget {
   const GoogleAuthButton({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Container(
       margin: const EdgeInsets.symmetric(vertical: 26), 
       width: 72,
@@ -24,13 +29,40 @@ class GoogleAuthButton extends StatelessWidget {
       ),
       child: Center(
         child: IconButton(
-          onPressed: () {}, 
+          onPressed: () {
+            execute(ref, context);
+          }, 
           icon: Container(
             padding: const EdgeInsets.all(16),
             child: Image.asset('images/google_logo.png'),
           )
         )
       )
+    );
+  }
+
+  void _showErrorSnackBar(context, message) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      ErrorSnackBar(message: message, context: context)
+    );
+  }
+
+  Future<void> execute(WidgetRef ref, BuildContext context) async {
+    await ref.read(googleAuthExecuteProvider.notifier).execute();
+    final googleAuth = ref.watch(googleAuthExecuteProvider);
+
+    googleAuth.when(
+      data: (data) {
+        if(data.isAuth == true) {
+          AutoRouter.of(context).pushAndPopUntil(const HomeRoute(), predicate: (route) => false);
+        }
+      },
+      error: (error, stack) {
+        _showErrorSnackBar(context, error.toString());
+      },
+      loading: () {
+        // 
+      }
     );
   }
 }
